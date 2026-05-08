f356587bddfbf79b3717c98a3ae6fad1adb4d78d
amell28 <siti24si@mahasiswa.pcr.ac.id>
2026-05-08 16:05:10 +0700

    Pertemuan 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,86 @@
-import { useState } from "react";
+import React, { Suspense, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import Loading from "./components/Loading";
 
-import Sidebar from "./layouts/Sidebar";
-import Header from "./layouts/Header";
-import Dashboard from "./pages/Dashboard";
-import Details from "./pages/Details";
-import Service from "./pages/Service";
-import NotFound from "./pages/NotFound";
-import Customers from "./pages/Customers";
-import Orders from "./pages/Orders";
-import ErrorPage from "./pages/ErrorPage";
+// import Dashboard from "./pages/Dashboard";
+// import Details from "./pages/Details";
+// import Service from "./pages/Service";
+// import NotFound from "./pages/NotFound";
+// import Customers from "./pages/Customers";
+// import Orders from "./pages/Orders";
+// import ErrorPage from "./pages/ErrorPage";
+// import MainLayout from "./layouts/MainLayout";
+// import AuthLayout from "./layouts/AuthLayout";
+// import Login from "./pages/auth/Login";
+// import Register from "./pages/auth/Register";
+// import Forgot from "./pages/auth/Forgot";
+
+const Dashboard = React.lazy(() => import("./pages/Dashboard"))
+const Details = React.lazy(() => import("./pages/Details"))
+const Service = React.lazy(() => import("./pages/Service"))
+const Customers = React.lazy(() => import("./pages/Customers"))
+const Orders = React.lazy(() => import("./pages/Orders"))
+const ErrorPage = React.lazy(() => import("./pages/ErrorPage"))
+const MainLayout = React.lazy(() => import("./layouts/MainLayout"))
+const AuthLayout = React.lazy(() => import("./layouts/AuthLayout"))
+const Login = React.lazy(() => import("./pages/auth/Login"))
+const Register = React.lazy(() => import("./pages/auth/Register"))
+const Forgot = React.lazy(() => import("./pages/auth/Forgot"))
+const NotFound = React.lazy(() => import("./pages/NotFound"))
 
 export default function App() {
   const [search, setSearch] = useState("");
 
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      <Sidebar />
+    <Suspense fallback={<Loading />}>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Dashboard search={search} />} />
+        <Route path="/details" element={<Details search={search} />} />
+        <Route path="/services" element={<Service search={search} />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/orders" element={<Orders />} />
 
-      <div className="flex-1 flex flex-col">
-        <Header search={search} setSearch={setSearch} />
-        <Routes>
-          <Route path="/" element={<Dashboard search={search} />} />
-          <Route path="/details" element={<Details search={search} />} />
-          <Route path="/services" element={<Service search={search} />} />
-          <Route path="/customers" element={<Customers />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route
-            path="/error-400"
-            element={
-              <ErrorPage
-                errorCode="400"
-                errorDescription="Permintaan tidak valid. Harap periksa kembali parameter yang Anda kirim."
-                errorImage="https://http.cat/400"
-              />
-            }
-          />
-          <Route
-            path="/error-401"
-            element={
-              <ErrorPage
-                errorCode="401"
-                errorDescription="Anda tidak terautentikasi. Silakan login terlebih dahulu untuk mengakses halaman ini."
-                errorImage="https://http.cat/401"
-              />
-            }
-          />
-          <Route
-            path="/error-403"
-            element={
-              <ErrorPage
-                errorCode="403"
-                errorDescription="Anda tidak memiliki izin untuk mengakses halaman ini. Akses ditolak."
-                errorImage="https://http.cat/403"
-              />
-            }
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </div>
+        <Route
+          path="/error-400"
+          element={
+            <ErrorPage
+              errorCode="400"
+              errorDescription="Permintaan tidak valid. Harap periksa kembali parameter yang Anda kirim."
+              errorImage="https://http.cat/400"
+            />
+          }
+        />
+        <Route
+          path="/error-401"
+          element={
+            <ErrorPage
+              errorCode="401"
+              errorDescription="Anda tidak terautentikasi. Silakan login terlebih dahulu untuk mengakses halaman ini."
+              errorImage="https://http.cat/401"
+            />
+          }
+        />
+        <Route
+          path="/error-403"
+          element={
+            <ErrorPage
+              errorCode="403"
+              errorDescription="Anda tidak memiliki izin untuk mengakses halaman ini. Akses ditolak."
+              errorImage="https://http.cat/403"
+            />
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+      
+
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot" element={<Forgot />} />
+      </Route>
+    </Routes>
+    </Suspense>
   );
 }
